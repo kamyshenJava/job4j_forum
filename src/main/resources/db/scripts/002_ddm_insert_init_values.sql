7ac7bf37148bfb5c7d367ca065dc2166bd7eb5b9
Vasilii <kamyshen@tutanota.com>
2022-09-11 04:51:29 +0300

    0. Spring Liquibase [#302330]

---
+++ src/main/resources/db/scripts/002_ddm_insert_init_values.sql
@@ -0,0 +1,7 @@
+insert into authorities (authority) values ('ROLE_USER'), ('ROLE_ADMIN')
+on conflict do nothing;
+
+insert into users (username, enabled, password, authority_id)
+values ('admin', true, '$2a$10$DysaOxXQZ3DvdnCl6ebn3.K/zTXBu/UF7y//w40wmLMODaepHmP2i',
+        (select id from authorities where authority = 'ROLE_ADMIN'))
+on conflict do nothing;
